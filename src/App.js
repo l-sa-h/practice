dabda2b1a1f9cd3a264f3446028bd2a6071d6a74
happy <but5555@naver.com>
2024-11-26 18:47:07 +0900

    느낌표!

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Provider, useDispatch, useSelector } from 'react-redux';
 function Home(){
   return(
     <div>
-      <h2>HOME22</h2>
+      <h2>HOME!!!!</h2>
       HOME is...
     </div>
   )
